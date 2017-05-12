a83e93a8009fea7ff92de5aa8394de3b1ce499f5
WIN_NT\x16j018 <x16j018@chiba-fjb.ac.jp>
2017-05-12 12:26:59 +0900

    表示方法を　getType().nameを利用するものにした

--- Program.cs
+++ Program.cs
@@ -10,36 +10,38 @@ namespace Example7
     {
         static void Main(string[] args)
         {
-            sbyte valueSByte = -100;
+
+            sbyte valueSbyte = -100;
             byte valueByte = 100;
-            short valudShort = -20000;
-            ushort vaueUShort = 50000;
+            short valueShort = -20000;
+            ushort valueUShort = 50000;
             int valueInt = -1000000000;
             uint valueUInt = 4000000000;
-            long valueLong = -80000000000000;
-            ulong valueULong = 90000000000000000;
+            long valueLong = -800000000000;
+            ulong valueULong = 900000000000000000;
 
             float valueFloat = 3.1415F;
-            double valueDouble = 3.1415926535;
+            double valueDouble = 3.141596535;
 
             char valueChar = 'A';
             string valueString = "C# PROGRAM";
 
             bool valueBool = true;
 
-            Console.WriteLine("sbyte : {0}", valueSByte);
-            Console.WriteLine("byte: {0}", valueByte);
-            Console.WriteLine("short: {0}", valudShort);
-            Console.WriteLine("ushort: {0}", vaueUShort);
-            Console.WriteLine("int: {0}", valueInt);
-            Console.WriteLine("uint: {0}", valueUInt);
-            Console.WriteLine("long: {0}", valueLong);
-            Console.WriteLine("ulong: {0}", valueULong);
-            Console.WriteLine("float: {0}", valueFloat);
-            Console.WriteLine("double: {0}", valueDouble);
-            Console.WriteLine("char: {0}", valueChar);
-            Console.WriteLine("string: {0}", valueString);
-            Console.WriteLine("bool: {0}", valueBool);
+            Console.WriteLine("{1} :{0}", valueSbyte,valueSbyte.GetType().Name);
+            Console.WriteLine("{1} :{0}", valueByte, valueByte.GetType().Name);
+            Console.WriteLine("{1} :{0}", valueShort, valueShort.GetType().Name);
+            Console.WriteLine("{1} :{0}", valueUShort, valueUShort.GetType().Name);
+            Console.WriteLine("{1} :{0}", valueInt, valueInt.GetType().Name);
+            Console.WriteLine("{1} :{0}", valueUInt, valueUInt.GetType().Name);
+            Console.WriteLine("{1} :{0}", valueLong, valueLong.GetType().Name);
+            Console.WriteLine("{1} :{0}", valueULong, valueULong.GetType().Name);
+            Console.WriteLine("{1} :{0}", valueFloat, valueFloat.GetType().Name);
+            Console.WriteLine("{1} :{0}", valueDouble, valueDouble.GetType().Name);
+            Console.WriteLine("{1} :{0}", valueChar, valueChar.GetType().Name);
+            Console.WriteLine("{1} :{0}", valueString, valueString.GetType().Name);
+            Console.WriteLine("{1} :{0}", valueBool, valueBool.GetType().Name);
+
         }
     }
 }
